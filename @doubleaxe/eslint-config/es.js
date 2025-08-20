3d2370aa53718fe5df0cbba4ec5d1aacb5e42125
Alexey Usov <dax@xdax.ru>
2025-08-21 01:18:37 +0700

    some es configs

---
+++ @doubleaxe/eslint-config/es.js
@@ -0,0 +1,281 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
+
+import root from './root.js';
+
+/**
+ * @typedef EslintConfig
+ * @type {import("eslint").Linter.Config}
+ */
+/**
+ * @typedef EslintRules
+ * @type {import("eslint").Linter.RulesRecord}
+ */
+
+/** @type {EslintRules} */
+const esNextRulesBase = {
+    'array-callback-return': [
+        'error',
+        {
+            allowImplicit: true,
+            checkForEach: false,
+            allowVoid: false,
+        },
+    ],
+    'block-scoped-var': ['error'],
+    'camelcase': [
+        'error',
+        {
+            properties: 'never',
+            ignoreDestructuring: false,
+            ignoreImports: false,
+            ignoreGlobals: false,
+        },
+    ],
+    'consistent-return': ['error'],
+    'curly': ['error', 'multi-line'],
+    'default-case-last': ['error'],
+    'default-param-last': ['error'],
+    'dot-notation': [
+        'error',
+        {
+            allowKeywords: true,
+            allowPattern: '',
+        },
+    ],
+    'grouped-accessor-pairs': ['error'],
+    'no-alert': ['warn'],
+    'no-array-constructor': ['error'],
+    'no-caller': ['error'],
+    'no-constructor-return': ['error'],
+    'no-else-return': [
+        'error',
+        {
+            allowElseIf: true,
+        },
+    ],
+    'no-empty-function': [
+        'error',
+        {
+            allow: ['arrowFunctions', 'functions', 'methods'],
+        },
+    ],
+    'no-eq-null': ['error'],
+    'no-eval': ['error'],
+    'no-extend-native': ['error'],
+    'no-extra-bind': ['error'],
+    'no-extra-label': ['error'],
+    'no-implied-eval': ['error'],
+    'no-iterator': ['error'],
+    'no-label-var': ['error'],
+    'no-labels': [
+        'error',
+        {
+            allowLoop: false,
+            allowSwitch: false,
+        },
+    ],
+    'no-lone-blocks': ['error'],
+    'no-lonely-if': ['error'],
+    'no-loop-func': ['error'],
+    'no-multi-assign': ['error'],
+    'no-multi-str': ['error'],
+    'no-nested-ternary': ['error'],
+    'no-new': ['error'],
+    'no-new-func': ['error'],
+    'no-new-wrappers': ['error'],
+    'no-object-constructor': ['error'],
+    'no-octal-escape': ['error'],
+    'no-promise-executor-return': ['error'],
+    'no-proto': ['error'],
+    'no-restricted-globals': [
+        'error',
+        {
+            name: 'isFinite',
+            message: 'Use Number.isFinite instead https://github.com/airbnb/javascript#standard-library--isfinite',
+        },
+        {
+            name: 'isNaN',
+            message: 'Use Number.isNaN instead https://github.com/airbnb/javascript#standard-library--isnan',
+        },
+    ],
+    'no-return-assign': ['error', 'always'],
+    'no-script-url': ['error'],
+    'no-self-compare': ['error'],
+    'no-sequences': ['error'],
+    'no-shadow': ['error'],
+    'no-template-curly-in-string': ['error'],
+    'no-throw-literal': ['error'],
+    'no-undef-init': ['error'],
+    'no-unneeded-ternary': [
+        'error',
+        {
+            defaultAssignment: false,
+        },
+    ],
+    'no-unreachable-loop': [
+        'error',
+        {
+            ignore: [],
+        },
+    ],
+    'no-unused-expressions': [
+        'error',
+        {
+            allowShortCircuit: true,
+            allowTernary: true,
+            allowTaggedTemplates: false,
+            enforceForJSX: false,
+        },
+    ],
+    'no-unused-vars': [
+        'error',
+        {
+            vars: 'all',
+            args: 'after-used',
+            argsIgnorePattern: '^_\\d*$',
+            destructuredArrayIgnorePattern: '^_\\d*$',
+            caughtErrors: 'none',
+            ignoreRestSiblings: true,
+            reportUsedIgnorePattern: true,
+        },
+    ],
+    'no-use-before-define': [
+        'error',
+        {
+            functions: false,
+            classes: false,
+        },
+    ],
+    'no-useless-concat': ['error'],
+    'no-useless-return': ['error'],
+    'no-void': ['error'],
+    'object-shorthand': [
+        'error',
+        'always',
+        {
+            avoidQuotes: true,
+        },
+    ],
+    'one-var': ['error', 'never'],
+    'operator-assignment': ['error', 'always'],
+    'prefer-const': [
+        'error',
+        {
+            destructuring: 'all',
+            ignoreReadBeforeAssign: true,
+        },
+    ],
+    'prefer-promise-reject-errors': [
+        'error',
+        {
+            allowEmptyReject: true,
+        },
+    ],
+    'prefer-regex-literals': [
+        'error',
+        {
+            disallowRedundantWrapping: true,
+        },
+    ],
+    'radix': ['error'],
+    'require-await': 'error',
+    'unicode-bom': ['error', 'never'],
+    'yoda': ['error'],
+
+    'no-restricted-properties': [
+        'error',
+        {
+            object: 'arguments',
+            property: 'callee',
+            message: 'arguments.callee is deprecated',
+        },
+        {
+            property: '__defineGetter__',
+            message: 'Please use Object.defineProperty instead.',
+        },
+        {
+            property: '__defineSetter__',
+            message: 'Please use Object.defineProperty instead.',
+        },
+    ],
+    'no-var': ['error'],
+    'prefer-numeric-literals': ['error'],
+    'prefer-object-spread': ['error'],
+
+    'arrow-body-style': [
+        'error',
+        'as-needed',
+        {
+            requireReturnForObjectLiteral: false,
+        },
+    ],
+    'no-restricted-exports': [
+        'error',
+        {
+            restrictedNamedExports: ['default', 'then'],
+        },
+    ],
+    'no-useless-computed-key': ['error'],
+    'no-useless-constructor': ['error'],
+    'no-useless-rename': [
+        'error',
+        {
+            ignoreDestructuring: false,
+            ignoreImport: false,
+            ignoreExport: false,
+        },
+    ],
+    'prefer-arrow-callback': [
+        'error',
+        {
+            allowNamedFunctions: false,
+            allowUnboundThis: true,
+        },
+    ],
+    'prefer-exponentiation-operator': ['error'],
+    'prefer-rest-params': ['error'],
+    'prefer-spread': ['error'],
+    'prefer-template': ['error'],
+    'symbol-description': ['error'],
+};
+
+/** @type {EslintConfig} */
+const esNextBase = {
+    name: 'doubleaxe/esNext',
+    rules: {
+        ...esNextRulesBase,
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+    },
+    plugins: { 'simple-import-sort': simpleImportSortPlugin },
+    languageOptions: {
+        ecmaVersion: 2024,
+        sourceType: 'module',
+        globals: {
+            ...globals.es2024,
+        },
+        parserOptions: {
+            ecmaFeatures: {
+                impliedStrict: true,
+                jsx: true,
+            },
+        },
+    },
+    settings: {},
+};
+
+export default {
+    baseConfigs: {
+        ...root.baseConfigs,
+        esNextBase,
+    },
+    configs: {
+        ...root.configs,
+    },
+    patterns: root.patterns,
+    utils: root.utils,
+    globals,
+};
--- @doubleaxe/eslint-config/root.js
+++ @doubleaxe/eslint-config/root.js
@@ -1,40 +1,102 @@
-/** @type {import("eslint").Linter.FlatConfig[]} */
-const root = [
-    {
-        ignores: ['**/node_modules/', '.git/', '**/dist*/', '**/vendor/', '**/*.min.js'],
-    },
+import globals from 'globals';
+
+/**
+ * @typedef EslintConfig
+ * @type {import("eslint").Linter.Config}
+ */
+/**
+ * @typedef EslintRules
+ * @type {import("eslint").Linter.RulesRecord}
+ */
+
+const ignores = ['**/node_modules/', '.git/', '.hg/', '**/dist*/', '**/vendor/', '**/*.min.js'];
+
+const toolsEs = [
+    '**/eslint.config.{js,mjs}',
+    '**/.prettierrc.{js,mjs}',
+    '**/.pnpmfile.{js,mjs}',
+    '**/webpack.config.mjs',
+    '**/stylelint.config.{js,mjs}',
+    '**/postcss.config.{js,mjs}',
+    '**/tailwind.config.{js,mjs}',
+    '**/jest.config.{js,mjs}',
+    '**/vitest.config.{js,mjs}',
+    '**/karma.conf.{js,mjs}',
+    '**/vite.config.{js,mjs}',
+    '**/rollup.config.{js,mjs}',
+    '**/esbuild.config.{js,mjs}',
 ];
 
+const toolsTs = [
+    '**/jest.config.ts',
+    '**/vitest.config.ts',
+    '**/karma.conf.ts',
+    '**/vite.config.ts',
+    '**/rollup.config.ts',
+    '**/esbuild.config.ts',
+];
+
+const esExtensions = ['js', 'mjs', 'jsx'];
+const esFilter = esExtensions.map((ext) => `**/*.${ext}`);
+
+const tsExtensions = ['ts', 'mts', 'tsx'];
+const tsFilter = tsExtensions.map((ext) => `**/*.${ext}`);
+
+const vueExtensions = ['vue'];
+const vueFilter = vueExtensions.map((ext) => `**/*.${ext}`);
+
 const utils = {
-    extendFiles(configs, ...patterns) {
-        return utils.extendFilesIf(configs, undefined, ...patterns);
-    },
-    extendFilesIf(configs, testFn, ...patterns) {
+    /**
+     * @param {EslintConfig[]} configs
+     * @param {string[]} files
+     * @param {boolean | undefined} [replace]
+     * @returns {EslintConfig[]}
+     */
+    extendFiles(configs, files, replace) {
         return configs.map((config) => {
             const keys = Object.keys(config);
             if (keys.length === 1 && keys[0] === 'ignores') {
                 // global ignores
                 return config;
             }
+            if (replace) {
+                return {
+                    ...config,
+                    files,
+                };
+            }
             return {
                 ...config,
-                files: patterns.reduce((files, pattern) => {
-                    if (testFn && !files?.some((file) => testFn(file))) {
-                        return files;
-                    }
-                    if (files?.some((file) => file === pattern)) {
-                        return files;
-                    }
-                    return (files || []).concat([pattern]);
-                }, config.files),
+                files: (config.files ?? []).concat(files.filter((file) => !config.files?.includes(file))),
             };
         });
     },
 };
 
-module.exports = {
+/** @type {EslintConfig[]} */
+const root = [
+    {
+        name: 'doubleaxe/root',
+        ignores,
+    },
+];
+
+export default {
+    baseConfigs: {},
     configs: {
         root,
     },
+    patterns: {
+        ignores,
+        toolsEs,
+        toolsTs,
+        esExtensions,
+        tsExtensions,
+        vueExtensions,
+        esFilter,
+        tsFilter,
+        vueFilter,
+    },
     utils,
+    globals,
 };
